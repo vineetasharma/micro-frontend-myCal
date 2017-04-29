a6a867d56404a1255e5d33c50d8fb06f87fe4e1a
Vineeta Sharma <vineetasharma18791@gmail.com>
2017-04-29 12:21:41 +0530

    basic routes added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const SignUp = () => (
+  <div>
+    <h2>SignUp</h2>
+  </div>
+)
 
-export default App;
+const Login = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
+const Home = ({ match }) => (
+  <div>
+    <h2>Home</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Home}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Login</Link></li>
+        <li><Link to="/signup">SignUp</Link></li>
+        {/*<li><Link to="/topics">Home</Link></li>*/}
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Login}/>
+      <Route path="/signup" component={SignUp}/>
+      <Route path="/home" component={Home}/>
+    </div>
+  </Router>
+)
+export default App
