bc5c2e07d0343516b394a43bbe8371f8ad3a15f1
guptakvgaurav <guptakvgaurav@gmail.com>
2017-05-03 00:13:35 +0530

    1. Added axios support for fetching data from server... 2. Material UI for liking and disliking events.

--- src/Events/styles.js
+++ src/Events/styles.js
@@ -1,14 +1,18 @@
 /**
  * Created by ttn on 29/04/17.
  */
-const styles ={
+const styles = {
     eventItem: {
-    listStyleType: 'none',
-    marginBottom: 20,
+        listStyleType: 'none',
+        marginBottom: 20,
         marginTop: 20,
-    marginLeft: 50,
-    marginRight: 50,
-},
+        marginLeft: 50,
+        marginRight: 50,
+    },
+    loader: {
+        margin: 250,
+        marginLeft: 600
+    }
 };
 
 export  default  styles;
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -5,8 +5,6 @@ import thunk from 'redux-thunk';
 import { applyMiddleware } from 'redux';
 
 const logger = store => next => action => {
-    "use strict";
-    console.log(`Logger -> ${next}`);
     next(action);
 };
 
