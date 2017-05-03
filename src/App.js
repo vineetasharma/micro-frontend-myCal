a396ed3118b4cb33980b0d3aeac7cb2ff0cb290f
guptakvgaurav <guptakvgaurav@gmail.com>
2017-05-03 13:39:14 +0530

    1. Added Application navbar and side menu for better accessibility.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,96 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {Drawer, MenuItem, AppBar, FloatingActionButton} from 'material-ui';
+import createBrowserHistory from 'history/createBrowserHistory'
+
 import './App.css';
 import Login from './Login/component';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import  SignUp from './Signup/component'
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-
 import  EventList from './Events/EventList';
-injectTapEventPlugin();
 
+
+injectTapEventPlugin();
+const browserHistory = createBrowserHistory();
 
 const style = {
-    marginRight: 20,
-    position: 'Fixed',
-    bottom: 10,
-    right: 10
-};
+        marginRight: 20,
+        position: 'Fixed',
+        bottom: 10,
+        right: 10
+    },
+    menuItems = [
+        {route: '/', text: 'Login'},
+        {route: 'signup', text: 'Signup'},
+        {route: 'eventlist', text: 'EventList'}
+    ],
+    appTitle = "Mi-Cal";
 
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.onAboutIconClicked = this.onAboutIconClicked.bind(this);
+        console.log(props);
+        // this.onAboutIconClicked = this.onAboutIconClicked.bind(this);
+        this.state = {
+            open: false
+        };
     }
 
-    onAboutIconClicked () {
+    _handleHamburgerClick(e) {
+        e.preventDefault();
+        this._toggleDrawer();
+    }
 
+    _toggleDrawer() {
+        this.setState({
+            open: !this.state.open
+        })
+    }
+
+    _handleMenuItemClick(e, menuItem) {
+        this._toggleDrawer();
     }
 
     render() {
         return (
-            <Router>
+            <Router history={browserHistory}>
                 <div>
-                    <FloatingActionButton style={style} onTouchTap={this.onAboutIconClicked}>
-                        About
-                    </FloatingActionButton>
+                    <AppBar
+                        onLeftIconButtonTouchTap={e => this._handleHamburgerClick(e)}
+                        title={appTitle}
+                        iconClassNameRight="muidocs-icon-navigation-expand-more"
+                    />
+                    <Drawer
+                        open={this.state.open}
+                        ref="drawer"
+                        menuItems={menuItems}>
+                        <AppBar title={appTitle}
+                                onLeftIconButtonTouchTap={e => this._handleHamburgerClick(e)}
+                        />
+                        {
+                            menuItems.map((menuItem, index) => (
+                                <MenuItem
+                                    containerElement={<Link to={menuItem.route} />}
+                                    key={index}
+                                    onTouchTap={(e) => this._handleMenuItemClick(e, menuItem)}
+                                > { menuItem.text }</MenuItem>
+                            ))
+                        }
+                    </Drawer>
+                    {/*<FloatingActionButton style={style} onTouchTap={this.onAboutIconClicked}>*/}
+                    {/*About*/}
+                    {/*</FloatingActionButton>*/}
 
-                    <div className="nav">
-                        <ul>
-                            <li><Link to="/">Login</Link></li>
-                            <li><Link to="/signup">SignUp</Link></li>
-                            <li><Link to="/eventlist">LIst</Link></li>
-                        </ul>
-                    </div>
+                    {/*<div className="nav">*/}
+                    {/*<ul>*/}
+                    {/*<li><Link to="/">Login</Link></li>*/}
+                    {/*<li><Link to="/signup">SignUp</Link></li>*/}
+                    {/*<li><Link to="/eventlist">LIst</Link></li>*/}
+                    {/*</ul>*/}
+                    {/*</div>*/}
                     <Route exact path="/" component={Login}/>
                     <Route path="/signup" component={SignUp}/>
                     <Route path="/eventlist" component={EventList}/>
